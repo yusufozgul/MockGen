7b56f68c7e9c80e090c0131d5918ff8702ee26c3
Yusuf Özgül <yusuf.ozgul@trendyol.com>
2022-06-04 22:33:08 +0300

    Add spy generator unit tests

--- MockGeneratorTests/MockGeneratorTests.swift
+++ MockGeneratorTests/MockGeneratorTests.swift
@@ -16,6 +16,314 @@ class MockGeneratorTests: XCTestCase {
         super.setUp()
         xcode = .init()
         spyGenerator = .init(buffer: xcode)
+        xcode.stubbedIndentationWidth = 4
+        xcode.stubbedUsesTabsForIndentation = false
     }
 
+    func test_generate_JustVariable_ReturnMock() throws {
+        let file = """
+class TestClass: TestProtocol {
+    var someVariable: Data
+}
+"""
+
+        let mock = """
+class TestClass: TestProtocol {
+    var invokedSomeVariableSetter = false
+    var invokedSomeVariableSetterCount = 0
+    var invokedSomeVariable: Data?
+    var invokedSomeVariableList: [Data] = []
+    var invokedSomeVariableGetter = false
+    var invokedSomeVariableGetterCount = 0
+    var stubbedSomeVariable: Data!
+    var someVariable: Data {
+        set {
+            invokedSomeVariableSetter = true
+            invokedSomeVariableSetterCount += 1
+            invokedSomeVariable = newValue
+            invokedSomeVariableList.append(newValue)
+        }
+        get {
+            invokedSomeVariableGetter = true
+            invokedSomeVariableGetterCount += 1
+            return stubbedSomeVariable
+        }
+    }
+
+}
+
+"""
+
+        XCTAssertEqual(try spyGenerator.generate(source: file), mock)
+    }
+
+    func test_generate_JustSimpleFunction_ReturnMock() throws {
+        let file = """
+class TestClass: TestProtocol {
+    func setValue() {
+        <#code#>
+    }
+}
+"""
+
+        let mock = """
+class TestClass: TestProtocol {
+    var invokedSetValue = false
+    var invokedSetValueCount = 0
+    func setValue() {
+        invokedSetValue = true
+        invokedSetValueCount += 1
+    }
+
+}
+
+"""
+
+        XCTAssertEqual(try spyGenerator.generate(source: file), mock)
+    }
+
+    func test_generate_JustInputFunction_ReturnMock() throws {
+        let file = """
+class TestClass: TestProtocol {
+    func setValue(data: NSObject) {
+        <#code#>
+    }
+}
+"""
+
+        let mock = """
+class TestClass: TestProtocol {
+    var invokedSetValue = false
+    var invokedSetValueCount = 0
+    var invokedSetValueParameters: (data: NSObject, Void)?
+    var invokedSetValueParametersList: [(data: NSObject, Void)] = []
+    func setValue(data: NSObject) {
+        invokedSetValue = true
+        invokedSetValueCount += 1
+        invokedSetValueParameters = (data, ())
+        invokedSetValueParametersList.append((data, ()))
+    }
+
+}
+
+"""
+
+        XCTAssertEqual(try spyGenerator.generate(source: file), mock)
+    }
+
+    func test_generate_JustOutputFunction_ReturnMock() throws {
+        let file = """
+class TestClass: TestProtocol {
+    func getValue() -> Error {
+        <#code#>
+    }
+}
+"""
+
+        let mock = """
+class TestClass: TestProtocol {
+    var invokedGetValue = false
+    var invokedGetValueCount = 0
+    var stubbedGetValueResult: Error!
+    func getValue() -> Error {
+        invokedGetValue = true
+        invokedGetValueCount += 1
+        return stubbedGetValueResult
+    }
+
+}
+
+"""
+
+        XCTAssertEqual(try spyGenerator.generate(source: file), mock)
+    }
+
+    func test_generate_JustClosureFunction_ReturnMock() throws {
+        let file = """
+class TestClass: TestProtocol {
+    func fetchValue(completion: (String) -> Void) {
+        <#code#>
+    }
+}
+"""
+
+        let mock = """
+class TestClass: TestProtocol {
+    var invokedFetchValue = false
+    var invokedFetchValueCount = 0
+    var stubbedFetchValueCompletionResult: (String, Void)?
+    func fetchValue(completion: (String) -> Void) {
+        invokedFetchValue = true
+        invokedFetchValueCount += 1
+        if let result = stubbedFetchValueCompletionResult {
+            _ = completion(result.0)
+        }
+    }
+
+}
+
+"""
+
+        XCTAssertEqual(try spyGenerator.generate(source: file), mock)
+    }
+
+    func test_generate_WithDefaultValue_ReturnMock() throws {
+        let file = """
+class TestClass: TestProtocol {
+    var testString: String
+    var testInt: Int
+    var testDouble: Double
+    var testFloat: Float
+    var testCGFloat: CGFloat
+    var testBool: Bool
+
+    func fetchValue() -> Bool {
+        <#code#>
+    }
+}
+"""
+
+        let mock = """
+class TestClass: TestProtocol {
+    var invokedTestStringSetter = false
+    var invokedTestStringSetterCount = 0
+    var invokedTestString: String?
+    var invokedTestStringList: [String] = []
+    var invokedTestStringGetter = false
+    var invokedTestStringGetterCount = 0
+    var stubbedTestString: String! = ""
+    var testString: String {
+        set {
+            invokedTestStringSetter = true
+            invokedTestStringSetterCount += 1
+            invokedTestString = newValue
+            invokedTestStringList.append(newValue)
+        }
+        get {
+            invokedTestStringGetter = true
+            invokedTestStringGetterCount += 1
+            return stubbedTestString
+        }
+    }
+
+    var invokedTestIntSetter = false
+    var invokedTestIntSetterCount = 0
+    var invokedTestInt: Int?
+    var invokedTestIntList: [Int] = []
+    var invokedTestIntGetter = false
+    var invokedTestIntGetterCount = 0
+    var stubbedTestInt: Int! = 0
+    var testInt: Int {
+        set {
+            invokedTestIntSetter = true
+            invokedTestIntSetterCount += 1
+            invokedTestInt = newValue
+            invokedTestIntList.append(newValue)
+        }
+        get {
+            invokedTestIntGetter = true
+            invokedTestIntGetterCount += 1
+            return stubbedTestInt
+        }
+    }
+
+    var invokedTestDoubleSetter = false
+    var invokedTestDoubleSetterCount = 0
+    var invokedTestDouble: Double?
+    var invokedTestDoubleList: [Double] = []
+    var invokedTestDoubleGetter = false
+    var invokedTestDoubleGetterCount = 0
+    var stubbedTestDouble: Double! = 0.0
+    var testDouble: Double {
+        set {
+            invokedTestDoubleSetter = true
+            invokedTestDoubleSetterCount += 1
+            invokedTestDouble = newValue
+            invokedTestDoubleList.append(newValue)
+        }
+        get {
+            invokedTestDoubleGetter = true
+            invokedTestDoubleGetterCount += 1
+            return stubbedTestDouble
+        }
+    }
+
+    var invokedTestFloatSetter = false
+    var invokedTestFloatSetterCount = 0
+    var invokedTestFloat: Float?
+    var invokedTestFloatList: [Float] = []
+    var invokedTestFloatGetter = false
+    var invokedTestFloatGetterCount = 0
+    var stubbedTestFloat: Float! = 0.0
+    var testFloat: Float {
+        set {
+            invokedTestFloatSetter = true
+            invokedTestFloatSetterCount += 1
+            invokedTestFloat = newValue
+            invokedTestFloatList.append(newValue)
+        }
+        get {
+            invokedTestFloatGetter = true
+            invokedTestFloatGetterCount += 1
+            return stubbedTestFloat
+        }
+    }
+
+    var invokedTestCGFloatSetter = false
+    var invokedTestCGFloatSetterCount = 0
+    var invokedTestCGFloat: CGFloat?
+    var invokedTestCGFloatList: [CGFloat] = []
+    var invokedTestCGFloatGetter = false
+    var invokedTestCGFloatGetterCount = 0
+    var stubbedTestCGFloat: CGFloat! = 0.0
+    var testCGFloat: CGFloat {
+        set {
+            invokedTestCGFloatSetter = true
+            invokedTestCGFloatSetterCount += 1
+            invokedTestCGFloat = newValue
+            invokedTestCGFloatList.append(newValue)
+        }
+        get {
+            invokedTestCGFloatGetter = true
+            invokedTestCGFloatGetterCount += 1
+            return stubbedTestCGFloat
+        }
+    }
+
+    var invokedTestBoolSetter = false
+    var invokedTestBoolSetterCount = 0
+    var invokedTestBool: Bool?
+    var invokedTestBoolList: [Bool] = []
+    var invokedTestBoolGetter = false
+    var invokedTestBoolGetterCount = 0
+    var stubbedTestBool: Bool! = false
+    var testBool: Bool {
+        set {
+            invokedTestBoolSetter = true
+            invokedTestBoolSetterCount += 1
+            invokedTestBool = newValue
+            invokedTestBoolList.append(newValue)
+        }
+        get {
+            invokedTestBoolGetter = true
+            invokedTestBoolGetterCount += 1
+            return stubbedTestBool
+        }
+    }
+
+    var invokedFetchValue = false
+    var invokedFetchValueCount = 0
+    var stubbedFetchValueResult: Bool!
+    func fetchValue() -> Bool {
+        invokedFetchValue = true
+        invokedFetchValueCount += 1
+        return stubbedFetchValueResult
+    }
+
+}
+
+"""
+
+        XCTAssertEqual(try spyGenerator.generate(source: file), mock)
+    }
 }
